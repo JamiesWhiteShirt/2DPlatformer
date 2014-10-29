f04e3bf3533227d3133067efb88cf131ab30deb9
JamiesWhiteShirt <erlend@aamdal.com>
2014-10-29 12:56:28 +0100

    Added spike

---
+++ Assets/Scripts/Spike.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spike : MonoBehaviour
+{
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (PlayerController.IsMe(collision.gameObject))
+		{
+			PlayerController.Kill();
+		}
+	}
+}
