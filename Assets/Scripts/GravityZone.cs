842ff6001be16c7746187bd4fac2b598b46769ce
JamiesWhiteShirt <erlend@aamdal.com>
2014-11-04 12:30:58 +0100

    I don't remember

--- Assets/Scripts/GravityZone.cs
+++ Assets/Scripts/GravityZone.cs
@@ -6,25 +6,23 @@ public class GravityZone : MonoBehaviour
 {
 	void Start ()
 	{
-		Bounds bounds = GetComponent<BoxCollider2D>().bounds;
-		Vector3 min = bounds.center - transform.position - bounds.size / 2.0f;
-		Vector3 max = min + bounds.size;
+		Vector3 size = GetComponent<BoxCollider2D>().bounds.size;
 
 		Mesh mesh = new Mesh();
 
 		mesh.vertices = new Vector3[]
 		{
-			new Vector3(min.x, min.y, 0.0f),
-			new Vector3(min.x, max.y, 0.0f),
-			new Vector3(max.x, max.y, 0.0f),
-			new Vector3(max.x, min.y, 0.0f)
+			new Vector3(-0.5f, -0.5f, 0.0f),
+			new Vector3(-0.5f, 0.5f, 0.0f),
+			new Vector3(0.5f, 0.5f, 0.0f),
+			new Vector3(0.5f, -0.5f, 0.0f)
 		};
 		mesh.uv = new Vector2[]
 		{
-			new Vector2(min.x, min.y),
-			new Vector2(min.x, max.y),
-			new Vector2(max.x, max.y),
-			new Vector2(max.x, min.y)
+			new Vector2(0.0f, 0.0f),
+			new Vector2(0.0f, size.y),
+			new Vector2(size.x, size.y),
+			new Vector2(size.x, 0.0f)
 		};
 		mesh.triangles = new int[]
 		{
